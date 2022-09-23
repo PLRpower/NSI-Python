509f1c1ac589289a36e82be993ffd97343232e56
plr <paul.thomas.stras@gmail.com>
2022-09-23 10:06:03 +0200

    Commit 23 Septembre (cours)

--- ex03_prog_objet.py
+++ ex03_prog_objet.py
@@ -26,22 +26,69 @@ class Point:
         return "(" + str(x) + ";" + str(y) + ")"
 
 
+# Exercice 2
 class Cercle:
 
     def __init__(self, centre, rayon):
         self._centre = centre
-        self._r = rayon
+        self._rayon = rayon
 
     def perimetre(self):
-        return self._r * 2
+        return pi * self._rayon * 2
 
     def surface(self):
-        return pi * self._r * self._r
+        return pi * self._rayon ** 2
 
     def __str__(self):
-        return "(" + self._r + ";" + self._centre + ")"
+        return Point.__str__(Point(self._rayon, self._centre))
+
+    def appartient(self, p):
+        return True if p.distance(self._centre) == self._rayon else False
 
 
-cercle = Cercle((1, 0), 5)
-print(cercle.perimetre())
-print(cercle.surface())
+# Exercice 3
+class Date:
+
+    def __init__(self, jour, mois, annee):
+        self._jour = jour
+        self._mois = mois
+        self._annee = annee
+
+    def mois(self):
+        liste = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet",
+                 "août", "septembre", "octobre", "novembre", "décembre"]
+        return liste[self._mois - 1]
+
+    def __str__(self):
+        return str(self._jour) + " " + self.mois() + " " + str(self._annee)
+
+    def __lt__(self, d2):
+        jour, mois, annee = d2
+        if self._annee < annee:
+            return True
+        elif self._annee > annee:
+            return False
+        elif self._mois < mois:
+            return True
+        elif self._mois > mois:
+            return False
+        elif self._jour < jour:
+            return True
+        else:
+            return False
+
+
+# Exercice 4
+class Tableau:
+
+    def __init__(self, imin, imax, v):
+        self._imin = imin
+        self._imax = imax
+        self._tab = [imin, imax]
+        self._v = v
+
+    def __len__(self):
+        return self._imin * self._imax
+
+    def __getitem__(self, item):
+        return item
