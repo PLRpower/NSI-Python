badfbe1babac461c930b37dd12e078c13c15c53d
plr <paul.thomas.stras@gmail.com>
2022-09-27 14:53:14 +0200

    Commit 27 Septembre (cours)

--- ex03_prog_objet.py
+++ ex03_prog_objet.py
@@ -115,5 +115,79 @@ class Tableau:
 class TaBiDir:
 
     def __init__(self, g, d):
-        self._d = d
-        self._g = g
+        self.g = g
+        self.d = d
+
+    def imin(self):
+        return - len(self.g)
+
+    def imax(self):
+        return len(self.d) - 1
+
+    def append(self, v):
+        self.d.append(v)
+
+    def prepend(self, v):
+        self.g = self.g.insert(0, v)
+
+    def __getitem__(self, item):
+        if self.imin() <= 1 <= self.imax():
+            if item >= 0:
+                return self.d[item]
+            return self.g[item]
+        raise IndexError(item)
+
+    def __setitem__(self, key, value):
+        if self.imin() <= 1 <= self.imax():
+            if key >= 0:
+                self.d[key] = value
+            self.g[key] = value
+        raise IndexError(key)
+
+    def __str__(self):
+        return str(self.g + self.d)
+
+
+t = TaBiDir([1, 2, 3], [5, 6, 7])
+print(t)
+
+
+class Carte:
+    """Initialise Couleur (entre 1 à 4), et Valeur (entre 1 à 13)"""
+
+    def __init__(self, c, v):
+        self.Couleur = c
+        self.Valeur = v
+
+    """Renvoie le nom de la Carte As, 2, ... 10, Valet, Dame, Roi"""
+
+    def getNom(self):
+        if (self.Valeur > 1 and self.Valeur < 11):
+            return str(self.Valeur)
+        elif self.Valeur == 11:
+            return "Valet"
+        elif self.Valeur == 12:
+            return "Dame"
+        elif self.Valeur == 13:
+            return "Roi"
+        else:
+            return "As"
+
+    """Renvoie la couleur de la Carte (pique, coeur, carreau, trefle)"""
+
+    def getCouleur(self):
+        return ['pique', 'coeur', 'carreau', 'trefle'][self.Couleur - 1]
+
+
+class PaquetDeCarte:
+
+    def __init__(self):
+        self.contenu = []
+
+    """Remplit le paquet de cartes"""
+    # def remplir(self):
+    # A compléter
+
+    """Renvoie la Carte qui se trouve à la position donnée"""
+    # def getCarteAt(self, pos):
+    # A compléter
