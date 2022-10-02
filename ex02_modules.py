a0b706c99ef4755e908ba5c749bc9bae494c4ab6
pault <paul.thomas.stras@gmail.com>
2022-10-02 10:36:38 +0200

    Avancement sur le sudoku

--- ex02_modules.py
+++ ex02_modules.py
@@ -1,10 +1,10 @@
-# Imports
+# ----- Imports -----
 from tkinter import *
-
 import requests
 from PIL import Image
 
-# Exercice 1
+
+# ----- Exercice 1 -----
 default_image = Image.open('data/in/tiger.jpg')  # Charger l'image par défaut du tigre
 
 image1 = default_image.rotate(180)
@@ -14,30 +14,34 @@ image1.close()  # Tourner l'image à 180 degrès, l'enregistrer et la fermer
 r, g, b = default_image.split()
 image2 = Image.merge("RGB", (g, r, b))
 image2.save('data/out/image2.jpg')
-image2.close()
+image2.close()  # Inverser les valeurs de couleur de l'image, l'enregistrer et la fermer
 
-# Exercice 2
-fenetre = Tk()
-fenetre.title("Petite fenêtre !")
-fenetre.geometry("400x400")
-fenetre.resizable(False, False)
-Button(text="Fermer la page", command=fenetre.destroy).pack()
-fenetre.mainloop()
 
-# Exercice 3
-data_people = requests.get("http://api.open-notify.org/astros.json").json()
-data_position = requests.get("http://api.open-notify.org/iss-now.json").json()
+# ----- Exercice 2 -----
+fenetre = Tk()  # Création de la fenêtre
+fenetre.title("Ma p'tite fenêtre")  # Définir un titre à la fenêtre
+fenetre.geometry("400x400")  # Définir la taille de la fenêtre
+fenetre.resizable(False, False)  # Bloquer le redimensionnement de la fenêtre
+Button(text="Fermer la page", command=fenetre.destroy).pack()  # Ajout d'un bouton, qui permet de fermer la fenêtre
 
-number = data_people['number']
+fenetre.mainloop() # Permet de lancer la fenêtre
+
+
+# ----- Exercice 3 -----
+data_people = requests.get("http://api.open-notify.org/astros.json").json()  # Permet des récupérer les astronautes via l'api externe
+number = data_people['number']  # Récupérer le nombre de personnes dans l'iss
 print("Nombre de personnes dans l'iss : " + str(number))
 for i in range(number):
     print(data_people['people'][i]['name'] + "se trouve dans : " + data_people['people'][i]['craft'])
+
+data_position = requests.get("http://api.open-notify.org/iss-now.json").json()  # Permet de récupérer la position de l'iss via l'api externe
 print("Longitude de l'iss : " + data_position['iss_position']['longitude'])
 print("Latitude de l'iss : " + data_position['iss_position']['latitude'])
 
 
-# Exercice 4
+# ----- Exercice 4 -----
 def population(nom):
+    # Permet de récupérer le nombre d'habitants d'une ville donnée via l'api externe
     data_commune = requests.get("https://geo.api.gouv.fr/communes?nom=" + str(nom) + "&fields=population").json()
     nombre_hab = data_commune[0]['population']
     print("Nombre d'habitants à " + str(nom) + " : " + str(nombre_hab))
--- sudoku.py
+++ sudoku.py
@@ -1,21 +1,32 @@
 # Nom des élèves du groupe : Paul THOMAS
+import tkinter.messagebox
+import platform
 
-class Grille:
+
+class Grid:
     def __init__(self):
-        self.contenu = [[[_p for _p in range(1, 10)] for _i in range(9)] for _j in range(9)]
+        self.table = [[[_p for _p in range(1, 10)] for _i in range(9)] for _j in range(9)]
 
     def __getitem__(self, item):
         i, j = item
-        return self.contenu[i][j]
+        return self.table[i][j]
 
     def __setitem__(self, key, value):
         i, j = key
-        self.contenu[i][j] = [value]
-        for a in range(9):
-            for b in range(9):
-                liste = self.contenu[a][b]
-                self[a][b] = liste.remove(value)
-                print(liste)
+        self.table[i][j] = value
+        rounded_value_i = i - (i % 3)
+        rounded_value_j = j - (j % 3)
+        [[self.erase(p, q, value) for p in range(rounded_value_i, rounded_value_i + 3)] for q in
+         range(rounded_value_j, rounded_value_j + 3)]
+
+        for p in range(9):
+            self.erase(i, p, value)
+            self.erase(p, j, value)
+
+    def erase(self, i, j, value):
+        if isinstance(self.table[i][j], list):
+            if value in self.table[i][j]:
+                self.table[i][j].remove(value)
 
 
 # Signaler la fin de la partie (perdue ou gagnée) et empêcher d’agir lorsque
@@ -30,123 +41,112 @@ class Grille:
 # Permettre de jouer sans « tricher », simplement en entrant la valeur au clavier
 #  sur une case donnée (choix du mode à choisir en début de partie)
 
-import platform
-
 if platform.mac_ver()[0] != '' and platform.python_version_tuple()[0] != '2':
     exit('TkInter crashes on this Mac with Python 3. Use Python 2.x')
-
 if platform.python_version_tuple()[0] == '2':
     import Tkinter as tk
 else:
     import tkinter as tk
 
-_tk = tk.Tk()
+_root = tk.Tk()
 
 
-class Affichage:
+class Display:
+
     def __init__(self):
-        # initialiser le tableau de cases contenant l'état de chaque case
         self.cases = [[None for _i in range(9)] for _j in range(9)]
-        # créer les 9 blocs, leurs cases et leurs sous-cases
-        [[self.creer_bloc(i, j) for i in range(3)] for j in range(3)]
-        _tk.title('Sudoku')
-        tk.mainloop()
+        [[self.create_block(i, j) for i in range(3)] for j in range(3)]
 
-    def creer_bloc(self, i, j):
-        """ Crée une grille de 3x3 blocs et la remplit de cases """
-        frame = tk.Frame(_tk, borderwidth=2, relief='sunken')
-        frame.grid(row=i, column=j, sticky='nsew')
+    def create_block(self, i, j):
+        f = tk.Frame(_root, borderwidth=2, relief='sunken')
+        f.grid(row=i, column=j, sticky='nsew')
         for n in range(3):
             for m in range(3):
-                self.cases[i * 3 + n][j * 3 + m] = self.creer_case(frame, i * 3 + n, j * 3 + m)
-        return frame
+                self.cases[i * 3 + n][j * 3 + m] = self.create_box(f, i * 3 + n, j * 3 + m)
+        return f
 
-    def creer_case(self, bloc, n, m):
-        """ Crée les 9 cases d'un bloc, chacune contenant 9 sous-cases pour les chiffres """
-        case = tk.Frame(bloc, borderwidth=2, relief='sunken')
+    def create_box(self, bloc, n, m):
+        case = tk.Frame(bloc, borderwidth=2, relief='groove')
         case.grid(row=n, column=m, sticky='nsew')
         for p in range(3):
             case.columnconfigure(p, minsize=20)
             case.rowconfigure(p, minsize=20)
-        chiffres = [None] * 9
+        numbers = [None] * 9
         for p in range(3):
             for q in range(3):
                 v = p * 3 + q + 1
-                ch = chiffres[v - 1] = tk.Label(case, text=str(v), fg='grey')
+                ch = numbers[v - 1] = tk.Label(case, text=str(v), fg='grey')
                 ch.grid(row=p, column=q, sticky='nsew')
                 # appeler self.click lorsque l'on clique sur une sous-case
-                ch.bind("<Button-1>", lambda ev, v=v: self.click(n, m, v))
+                ch.bind("<Button-1>", lambda ev, v=v: self.click_on_number(n, m, v))
         # une case est représentée par un dictionnaire avec :
         #   - son état : 'chiffres', 'definitif', 'erreur' (s'il n'y a plus de chiffres possibles)
         #   - le bloc auquel elle appartient
         #   - le widget Tk qui la représente
         #   - la liste des chiffres qu'elle contient (si type == 'chiffres') ou None (si type == 'definitif')
         return {
-            'type': 'chiffres',
+            'type': 'numbers',
             'bloc': bloc,
             'case': case,
-            'chiffres': chiffres
+            'numbers': numbers
         }
 
-    def afficher_valeur(self, i, j, v):
-        """ Afficher la valeur v dans la case i,j.
-            v peut être un tableau de valeurs possibles,
-            un chiffre définitif, ou None en cas d'erreur """
+    def display_value(self, i, j, v):
         case = self.cases[i][j]
         type = case['type']
         bloc = case['bloc']
-        chiffres = case['chiffres']
+        numbers = case['numbers']
 
-        if v == None or v == []:
+        if v is None or v == []:
             # pas de valeur : erreur dans la grille
             # détruire les sous-cases et afficher la case en rouge
-            if type != 'erreur':
-                case['type'] = 'erreur'
+            if type != 'error':
+                case['type'] = 'error'
                 case['case'].destroy
-                case['chiffres'] = None
-                erreur = case['case'] = tk.Frame(bloc, bg='red', borderwidth=2, relief='sunken')
-                erreur.grid(row=i, column=j, sticky='nsew')
+                case['numbers'] = None
+                error = case['case'] = tk.Frame(bloc, bg='red', borderwidth=2, relief='sunken')
+                error.grid(row=i, column=j, sticky='nsew')
+                tkinter.messagebox.askretrycancel(title='Défaite', message='Vous avez perdu ! Voulez rejouer ?')
         elif isinstance(v, int):
             # remplacer la grille de chiffres possibles par un chiffre définitif
             if type != 'definitif':
                 case['type'] = 'definitif'
                 case['case'].destroy
-                case['chiffres'] = None
-                chiffre = case['case'] = tk.Label(bloc, text=v, font=(None, 30), borderwidth=2, relief='sunken')
-                chiffre.grid(row=i, column=j, sticky='nsew')
+                case['numbers'] = None
+                number = case['case'] = tk.Label(bloc, text=v, font=(None, 30), borderwidth=2, relief='sunken')
+                number.grid(row=i, column=j, sticky='nsew')
         else:
             # mettre à jour les chiffres possibles
             # on "efface" les chiffres non possibles en les affichant en blanc
-            for ch in range(1, 10):
-                if ch in v:
-                    chiffres[ch - 1].config(fg='grey')
+            for ch in range(9):
+                if ch + 1 in v:
+                    numbers[ch].config(fg='grey')
                 else:
-                    chiffres[ch - 1].config(fg='white')
+                    numbers[ch].config(fg='white')
 
-    def click(self, i, j, v):
+    def click_on_number(self, i, j, v):
         case = self.cases[i][j]
-        chiffres = case['chiffres']
+        numbers = case['numbers']
+
         # ne rien faire s'il y a déjà un chiffre définitif
-        if chiffres == None:
+        if numbers is None:
             return
 
         # ne rien faire si on a cliqué sur un chiffre blanc, donc non autorisé
-        if chiffres:
-            color = chiffres[v - 1].cget('fg')
+        if numbers:
+            color = numbers[v - 1].cget('fg')
             if color == 'white':
                 return
 
         # affecter le chiffre à la case
-        grille[(i, j)] = v
+        grille[i, j] = v
 
         # mettre à jour la grille
         for i in range(9):
             for j in range(9):
-                self.afficher_valeur(i, j, grille[(i, j)])
+                self.display_value(i, j, grille[i, j])
 
 
-# -- programme principal
-
-# créer la grille
-grille = Grille()
-Affichage()
+grille = Grid()
+display = Display()
+tk.mainloop()
