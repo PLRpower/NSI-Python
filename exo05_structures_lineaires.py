43a9334934dc6485a5a66f04740987f8acb48402
plr <paul.thomas.stras@gmail.com>
2022-11-21 12:08:45 +0100

    Commit 21 novembre (cours)

--- exo05_structures_lineaires.py
+++ exo05_structures_lineaires.py
@@ -70,7 +70,7 @@ assert (calcP("1 2 3 * + 4 *") == 28.0)
 
 
 # Exercice 3
-def parenthese(s, f):
+def parentheses(s, f):
     pile = Pile()
     for i, x in enumerate(s):
         if i == f:
@@ -81,4 +81,127 @@ def parenthese(s, f):
             pile.depiler()
 
 
-print(parenthese(""))
+# Exercice 4
+def parentheses2(expr):
+    pile = Pile()
+    for i in expr:
+        if i == "(":
+            pile.empiler(i)
+        elif i == ")":
+            if pile.est_vide():
+                return False
+            if pile.consulter() == "(":
+                pile.depiler()
+
+    if pile.est_vide():
+        return True
+    return False
+
+
+# Exercice 5
+def simplifie(pile, ops):
+    pile = Pile()
+    while not pile.est_vide():
+        element = pile.consulter()
+        if element == "(":
+            pile.empiler(element)
+
+        elif element == ")":
+            return  ##
+
+        elif element == "+":
+            premier = pile.depiler()
+            deuxieme = pile.depiler()
+            pile.empiler(premier + deuxieme)
+
+        elif element == "*":
+            premier = pile.depiler()
+            deuxieme = pile.depiler()
+            pile.empiler(premier * deuxieme)
+
+        else:  # Si l'élément est un nombre
+            pile.empiler(element)
+
+
+# Exercice 6
+class PileBornee:
+    def __init__(self, c):
+        self.contenu = None
+        self._max = c
+        self._taille = 0
+
+    def est_vide(self):
+        return self.contenu is None
+
+    def est_pleine(self):
+        return self.contenu == self._max
+
+    def empiler(self, e):
+        if not self.est_pleine():
+            self.contenu = Cellule(e, self.contenu)
+            self._taille += 1
+        else:
+            raise IndexError('empiler sur une pile pleine')
+
+    def depiler(self):
+        if self.est_vide():
+            raise IndexError('dépiler sur une pile vide')
+        else:
+            self._taille -= 1
+            v = self.contenu.valeur
+            self.contenu = self.contenu.suivante
+            return v
+
+    def consulter(self):
+        if self.est_vide():
+            raise IndexError('consulter sur une pile vide')
+        else:
+            return self.contenu.valeur
+
+    def vider(self):
+        self.contenu = None
+
+    def taille(self):
+        return self._taille
+
+
+# Exercice 6
+class FileBornee:
+    def __init__(self, c):
+        self.contenu = None
+        self._max = c
+        self._taille = 0
+
+    def est_vide(self):
+        return self.contenu is None
+
+    def est_pleine(self):
+        return self.contenu == self._max
+
+    def empiler(self, e):
+        if not self.est_pleine():
+            self.contenu = Cellule(e, self.contenu)
+            self._taille += 1
+        else:
+            raise IndexError('empiler sur une pile pleine')
+
+    def depiler(self):
+        if self.est_vide():
+            raise IndexError('dépiler sur une pile vide')
+        else:
+            self._taille -= 1
+            v = self.contenu.valeur
+            self.contenu = self.contenu.suivante
+            return v
+
+    def consulter(self):
+        if self.est_vide():
+            raise IndexError('consulter sur une pile vide')
+        else:
+            return self.contenu.valeur
+
+    def vider(self):
+        self.contenu = None
+
+    def taille(self):
+        return self._taille
