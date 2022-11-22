9b490c6a055a0ec974c18f235962de34b9fd108c
plr <paul.thomas.stras@gmail.com>
2022-11-22 14:55:29 +0100

    Commit 22 novembre (cours)

--- exo05_structures_lineaires.py
+++ exo05_structures_lineaires.py
@@ -134,7 +134,7 @@ class PileBornee:
         return self.contenu is None
 
     def est_pleine(self):
-        return self.contenu == self._max
+        return self._taille == self._max
 
     def empiler(self, e):
         if not self.est_pleine():
@@ -168,7 +168,8 @@ class PileBornee:
 # Exercice 6
 class FileBornee:
     def __init__(self, c):
-        self.contenu = None
+        self.tete = None
+        self.queue = None
         self._max = c
         self._taille = 0
 
@@ -176,32 +177,102 @@ class FileBornee:
         return self.contenu is None
 
     def est_pleine(self):
-        return self.contenu == self._max
+        return self._taille == self._max
 
-    def empiler(self, e):
+    def ajouter(self, e):
         if not self.est_pleine():
-            self.contenu = Cellule(e, self.contenu)
+            c = Cellule(e, self.contenu)
+            if self.est_vide():
+                self.tete = c
+            else:
+                self.queue.suivante = c
+            self.queue = c
             self._taille += 1
         else:
-            raise IndexError('empiler sur une pile pleine')
+            raise IndexError('ajouter sur une file pleine')
 
-    def depiler(self):
+    def retirer(self):
         if self.est_vide():
-            raise IndexError('dépiler sur une pile vide')
+            raise IndexError('retirer sur une file vide')
         else:
             self._taille -= 1
-            v = self.contenu.valeur
-            self.contenu = self.contenu.suivante
+            v = self.tete.valeur
+            self.tete = self.tete.suivante
+            if self.tete is None:
+                self.queue = None
             return v
 
-    def consulter(self):
-        if self.est_vide():
-            raise IndexError('consulter sur une pile vide')
-        else:
-            return self.contenu.valeur
 
-    def vider(self):
-        self.contenu = None
+def creer_file(c):
+    return FileBornee(c)
 
-    def taille(self):
-        return self._taille
+
+def est_vide(p):
+    est_vide(p)
+
+
+def est_pleine(p):
+    est_pleine(p)
+
+
+def ajouter(p, e):
+    return p.ajouter(e)
+
+
+def retirer(p):
+    return p.retirer()
+
+
+def positif(T):
+    T2 = list(T)
+    T3 = list(T2)
+    while T2 != []:
+        x = T2.pop()
+        if x >= 0:
+            T3.append(x)
+    T2 = []
+    while T3 != []:
+        x = T3.pop()
+        if x >= 0:
+            T2.append(x)
+    print("T =", T)
+    return T2
+
+
+class Maillon:
+    def __init__(self, v):
+        self.valeur = v
+        self.suivant = None
+
+
+class File:
+    def __init__(self):
+        self.dernier_file = None
+
+    def enfile(self, element):
+        nouveau_maillon = Maillon(element)
+        nouveau_maillon.suivant = self.dernier_file
+        self.dernier_file = element
+
+    def est_vide(self):
+        return self.dernier_file is None
+
+    def affiche(self):
+        maillon = self.dernier_file
+        while maillon is not None:
+            print(maillon.valeur)
+            maillon =
+
+    def defile(self):
+        if not self.est_vide():
+            if self.dernier_file.suivant is None:
+                resultat = self.dernier_file.valeur
+                self.dernier_file = None
+                return resultat
+            maillon =
+            while maillon.suivant.suivant is not None:
+                maillon = maillon.suivant
+            resultat =
+            maillon.suivant = None
+            return resultat
+        return None
