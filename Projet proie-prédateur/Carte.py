49d4df490cf0df36465071ca4f94d9f6f76d62a2
plr <paul.thomas.stras@gmail.com>
2023-01-03 14:57:29 +0100

    Commit projet

---
+++ Projet proie-prédateur/Carte.py
@@ -0,0 +1,98 @@
+from tkinter import *
+
+from PIL import Image, ImageTk
+
+
+class Carte(Canvas):
+
+    def __init__(self, parent, params, **kwargs):
+        """Initialise le terrain (de type Canvas) en le plaçant dans la fenêtre"""
+        Canvas.__init__(self, parent, width=params.largeur_pixels, height=params.hauteur_pixels,
+                        bg="white", bd=0, **kwargs)
+
+        self.params = params
+
+        self.icons = {}
+        # Conservation en mémoire des images représentant les animaux
+        # Dictionnaire dont les clés sont les classes des animaux
+        # et les valeurs sont les images
+
+        self.herbe = []
+        # Tableau à deux dimensions de la taille du terrain
+        # contenant des listes de la forme [hauteur d’herbe, représentation]
+
+        self.animal = {}
+        # Dictionnaire dont les clés sont les couples (x,y)
+        # et les valeurs sont les couples (animal,représentation)
+
+        self.pack()
+
+    def reinit(self):
+        """
+        Réinitialise le terrain. À exécuter quand on veut recommencer
+        et après avoir modifié la largeur dans les paramètres
+        """
+        self.delete('all')
+
+        t_icone = self.params.taille_case - 2 * self.params.border
+        wolf = ImageTk.PhotoImage(Image.open('wolf_38.png').resize((t_icone, t_icone)))
+        rabbit = ImageTk.PhotoImage(Image.open('rabbit_38.png').resize((t_icone, t_icone)))
+        self.icons = {"loup": wolf, "lapin": rabbit}
+        # Conservation en mémoire des images représentant les animaux
+        # Dictionnaire dont les clés sont les classes des animaux
+        # et les valeurs sont les images
+
+        c, b = self.params.taille_case, self.params.border
+        h, l = self.params.hauteur, self.params.largeur
+
+        self.herbe = [[[self.params.niveau_herbe,
+                        self.create_rectangle(x * c + b, y * c + b, (x + 1) * c + b, (y + 1) * c + b,
+                                              fill=self.couleur(self.params.niveau_herbe),
+                                              width=b, outline='gray')]
+                       for y in range(h)] for x in range(l)]
+        # Tableau à deux dimensions de la taille du terrain
+        # contenant des listes de la forme [hauteur d’herbe, représentation]
+
+        self.animal = {}
+        # Dictionnaire dont les clés sont les couples (x,y)
+        # et les valeurs sont les couples (animal,représentation)
+
+    def couleur(self, value):
+        """Retourne la couleur associée à une hauteur d’herbe"""
+        return '#60' + hex(round(value * 1.1) + 70)[2:] + '00'
+
+    def faire_pousser_herbe(self):
+        """Fait pousser toute l’herbe du terrain"""
+        for x in range(self.params.largeur):
+            for y in range(self.params.hauteur):
+                if self.herbe[x][y][0] < 100:
+                    self.herbe[x][y][0] += 1
+                    self.itemconfig(self.herbe[x][y][1], fill=self.couleur(self.herbe[x][y][0]))
+
+    def place_animal(self, animal, x, y):
+        """Place un animal donné aux coordonnées (x,y) données"""
+        assert not (x, y) in self.animal.keys()
+        self.animal[(x, y)] = animal, self.create_image(self.coord_to_center(x), self.coord_to_center(y),
+                                                        image=self.icons[animal.classe])
+
+    def bouge_animal(self, x0, y0, x, y):
+        """Déplace un animal donné par ses coordonnées (x0,y0) aux coordonnées (x,y)"""
+        assert not (x, y) in self.animal.keys()
+        self.animal[(x, y)] = self.animal.pop((x0, y0))
+        self.coords(self.animal[(x, y)][1],
+                    self.coord_to_center(x), self.coord_to_center(y))
+
+    def elimine_animal(self, x, y):
+        """supprime un animal du terrain"""
+        _, rep = self.animal.pop((x, y))
+        self.delete(rep)
+
+    def coupe_herbe(self, x, y):
+        """Coupe l’herbe aux coordonnées données et retourne la quantité de nourriture"""
+        un = self.herbe[x][y][0]
+        self.herbe[x][y][0] = 0
+        self.itemconfig(self.herbe[x][y][1], fill=self.couleur(0))
+        return un
+
+    def coord_to_center(self, x):
+        return (2 * x + 1) * self.params.taille_case // 2 + self.params.border
--- projet_sudoku.py
+++ projet_sudoku.py
@@ -4,36 +4,30 @@
 # Projet Sudoku
 #
 
-# Nom des élèves du groupe :
+# Nom des élèves du groupe : Paul, Youssef
 
-# On veut représenter les valeurs possibles pour chaque case de la grille
-# (au début toutes les valeurs de 1 à 9) et mettre ces listes à jour
-# automatiquement lorsque l’on fixe la valeur d’une case.
 
 class Grille:
-    def __init__(self, grille=None):
+    def __init__(self, tableau=None):
         """ Initialise la grille avec le contenu de celle-ci """
-
-        if grille is None:  # S'il n'y a pas de grille définie au départ
-            self.tableau = [[[x for x in range(1, 10)] for _ in range(9)] for _ in
-                            range(9)]  # Créer un tableau bidirectionnel contenant des listes de 1 à 9.
-        else:  # Si la grille est déjà définie au départ
-            self.tableau = grille  # Définir le tableau par la grille choisie au départ
+        self.contenu = [[[x for x in range(1, 10)] for i in range(9)] for j in
+                        range(9)]  # Créer un tableau bidirectionnel contenant des listes de 1 à 9.
+        self.tableau = tableau  # Permet d'enregistrer le tableau optionnel
 
     def __getitem__(self, c):
         """ Retourne le chiffre affecté à la case c=(i,j),
         ou bien la liste des chiffres possibles de cette case """
 
         ligne, colonne = c  # Obtenir la ligne et la colonne de la case choisie.
-        return self.tableau[ligne][colonne]  # Renvoyer le contenu de cette case.
+        return self.contenu[ligne][colonne]  # Renvoyer le contenu de cette case.
 
     def __setitem__(self, c, v):
         """ Affecte la valeur v à la case c=(i,j)
         et met à jour l‘information sur la grille
-        selon les règles du sudoku"""
+        selon les règles du sudoku """
 
         ligne, colonne = c  # Obtenir la ligne et la colonne de la case choisie.
-        self.tableau[ligne][colonne] = v  # Remplacer le contenu de cette case par le contenu souhaité.
+        self.contenu[ligne][colonne] = v  # Remplacer le contenu de cette case par le contenu souhaité.
 
         [self.erase(ligne, x, v) for x in range(9)]  # Supprimer toutes les mêmes valeurs dans la ligne.
 
@@ -44,27 +38,12 @@ class Grille:
         [[self.erase(i, j, v) for i in range(box_x, box_x + 3)] for j in range(box_y, box_y + 3)]
         # Supprimer toutes les mêmes valeurs dans le carré.
 
-    def erase(self, ligne, colonne,
-              valeur):  # Fonction permettant de supprimer une valeur choisie dans une case choisie.
-        if isinstance(self.tableau[ligne][colonne],
+    def erase(self, ligne, colonne, valeur):
+        """ Fonction permettant de supprimer une valeur choisie dans une case choisie. """
+        if isinstance(self.contenu[ligne][colonne],
                       list):  # Si la case choisie est une liste (donc numéro non définitif).
-            if valeur in self.tableau[ligne][colonne]:  # Si la valeur choisie est dans la liste
-                self.tableau[ligne][colonne].remove(valeur)  # Supprimer cette valeur dans la case.
-
-
-##* Signaler la fin de la partie (perdue ou gagnée) et empêcher d’agir lorsque
-##  l’on clique sur une case alors que c’est fini
-##* Faire apparaître un bouton lorsque la partie se termine qui propose de recommencer,
-##  et recommencer quand on clique dessus (il faudra pour cela définir une fonction init())
-##* Permettre de charger une partie à partir d’une liste de valeurs préremplies ;
-##  Autrement dit mettre un argument supplémentaire à la fonction __init__
-##  de Grille et utiliser cela pour initialiser avec une grille de départ
-##* Résolution automatique (quand c’est possible) en cliquant sur un bouton
-##* Permettre de revenir en arrière (nécessite de garder un historique de ce qui a été fait)
-##* Permettre de jouer sans « tricher », simplement en entrant la valeur au clavier
-##  sur une case donnée (choix du mode à choisir en début de partie)
-##
-## Toute autre idée intéressante est bienvenue
+            if valeur in self.contenu[ligne][colonne]:  # Si la valeur choisie est dans la liste
+                self.contenu[ligne][colonne].remove(valeur)  # Supprimer cette valeur dans la case.
 
 # Eviter le bug qui plante MacOS avec TkInter et Python3 !
 import platform
@@ -96,6 +75,7 @@ class Affichage:
         self.cases = [[None for i in range(9)] for j in range(9)]
         # créer les 9 blocs, leurs cases et leurs sous-cases
         [[self.creer_bloc(i, j) for i in range(3)] for j in range(3)]
+        self.update_grid()  # Permet d'actualiser la grille, si elle est déjà prédéfinie
 
     def creer_bloc(self, i, j):
         """ Crée une grille de 3x3 blocs et la remplit de cases """
@@ -151,7 +131,7 @@ class Affichage:
                 case['chiffres'] = None
                 erreur = case['case'] = tk.Frame(bloc, bg='red', borderwidth=2, relief='sunken')
                 erreur.grid(row=i, column=j, sticky='nsew')
-                return 'erreur'  # Renvoyer le signal d'une erreur (pour signaler la fin de la partie)
+
         elif isinstance(v, int):
             # remplacer la grille de chiffres possibles par un chiffre définitif
             if type != 'definitif':
@@ -188,23 +168,31 @@ class Affichage:
         grille[(i, j)] = v
 
         # mettre à jour la grille
+        self.update_grid()
+
+        # Vérifier si le jeu est terminé
+        self.check_end()
+
+    def update_grid(self):
+        """ Fonction permettant d'actualiser/faire une mise à jour des cases """
+
         for i in range(9):
             for j in range(9):
                 self.afficher_valeur(i, j, grille[(i, j)])
 
-        self.check_end()
-
     def check_end(self):
+        """ Fonction permettant de vérifier si le jeu est terminé, appelée à chaque fois qu'on clique sur un numéro """
+
         end = True
         for i in range(9):
-            for j in range(9):
+            for j in range(9):  # Vérifier toutes les cases, avec leurs ligne et colonne.
                 case = self.cases[i][j]
                 type = case['type']
 
-                if type != 'definitif':
-                    end = False
+                if type != 'definitif':  # Si la case n'est pas définitive (s'il reste des cases vides).
+                    end = False  # Ne pas finir le jeu
 
-                if type == 'erreur':
+                if type == 'erreur':  # Si la case contient une erreur (si le jeu est perdu).
                     answer = tk.messagebox.askretrycancel('Défaite',
                                                           'Voulez-vous rejouer ?')  # Envoyer une alerte, proposant de rejouer.
                     if answer:  # Si il décide de rejouer.
@@ -213,18 +201,65 @@ class Affichage:
                         break  # Arrêter les boucles.
                     _root.destroy()  # Fermer la page.
                     break  # Arrêter les boucles.
-        if end is True:
+        if end is True:  # S'il y aucune erreur et que toutes les cases sont définitives.
             answer = tk.messagebox.askyesno('Victoire',
-                                            'Voulez-vous rejouer ?')  # Envoyer une alerte, proposant de rejouer.
+                                            'Voulez-vous rejouer ?')  # Envoyer une information, proposant de rejouer.
             if answer:  # Si il décide de rejouer.
                 grille.__init__()  # Régénérer la grille.
                 self.__init__()  # Régénérer l'affichage.
-            _root.destroy()  # Fermer la page.
+            else:
+                _root.destroy()  # Fermer la page.
 
 
-# créer la grille
-grille = Grille()
+def solve_sudoku():
+    """ Fonction permettant de résoudre automatiquement le sudoku, ne fonctionne malheuresement pas totalement encore """
+
+    for row in range(9):
+        for col in range(9):
+            for value in range(1, 10):
+                if isinstance(grille.contenu[row][col], list):
+                    for x in range(9):
+                        if isinstance(grille.contenu[row][x], list):
+                            if len(grille.contenu[row][x]) == 1:
+                                return
+
+                    for x in range(9):
+                        if isinstance(grille.contenu[x][col], list):
+                            if len(grille.contenu[x][col]) == 1:
+                                return
+
+                    rounded_row = row - row % 3
+                    rounded_col = col - col % 3
+                    for i in range(3):
+                        for j in range(3):
+                            if isinstance(grille.contenu[i + rounded_row][j + rounded_col], list):
+                                if len(list(grille.contenu[i + rounded_row][j + rounded_col])) == 1:
+                                    return
+                    ecran.click(row, col, value)
+
+
+# Créer la grille, avec ou non une grille prédéfinie. Si oui, les 0 ne vont être aucune valeur.
+grille = Grille([[7, 0, 3, 4, 5, 0, 0, 8, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 4, 0],
+                 [4, 0, 0, 8, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 3, 0, 0, 0, 0],
+                 [0, 5, 0, 0, 0, 0, 1, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 2, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 2, 0, 0]])
+
 # afficher la grille
 ecran = Affichage()
+
+# cliquez sur les cases prédéfinies par la grille (sans les 0)
+for i in range(9):
+    for j in range(9):
+        if grille.tableau[i][j] != 0:
+            ecran.click(i, j, grille.tableau[i][j])
+
 # boucle d'interaction
+tk.Button(text="Résoudre automatiqument'", bg='#50C878', activebackground='#00A36C',
+          command=lambda: solve_sudoku()).grid()
+_root.title('Sudoku - Paul et Youssef')
 tk.mainloop()
