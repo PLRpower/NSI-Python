681b0572e831e4b78cdb400fb50e9f76a081255b
plr <paul.thomas.stras@gmail.com>
2023-02-28 11:07:44 +0100

    Commit projet base de donnée

--- Projet base de donnée/main.py
+++ Projet base de donnée/main.py
@@ -5,64 +5,31 @@ from prettytable import PrettyTable
 
 
 def main():
-    num_etu = None
-    stop = False
-    while not stop:
-        print("", "---------", "Connexion", "---------", sep="\n")
+    # Connexion avec l'identifiant d'un étudiant
+    print("\n---------\nConnexion\n---------")
+    while True:
         num_etu = input("Saisissez votre numéro d'étudiant : ")
         connexion, curseur = ouvrir_connexion()
         curseur.execute("SELECT nom, prenom FROM Eleve WHERE num_etu LIKE ?", (num_etu,))
         etudiant = curseur.fetchone()
-        fermer_connexion(connexion)
-        if etudiant is None:
-            print("Numéro d'étudiant introuvable, veuillez réessayez.")
-        else:
-            print("Bienvenue dans le centre de documentation et d'information " + str(etudiant[0]) + " " + str(
-                etudiant[1]) + " !")
-            stop = True
-    stop = False
-    while not stop:
-        print("", "----", "Menu", "----",
-              "(d) rechercher un document",
-              "(e) emprunter",
-              "(r) rendre",
-              "(a) ajouter",
-              "(s) supprimer",
-              "(p) rechercher un prêt",
-              "(q) quitter", sep="\n")
+        connexion.close()
+        if etudiant:
+            print("Bienvenue dans le centre de documentation et d'information", etudiant[0], etudiant[1], "!")
+            break  # Arrêter la boucle While
+        print("\nNuméro d'étudiant introuvable, veuillez réessayez.")
+
+    # Menu
+    while True:
+        print("", "----", "Menu", "----", "(d) rechercher un document", "(e) emprunter", "(r) rendre", "(a) ajouter",
+              "(s) supprimer", "(p) rechercher un prêt", "(q) quitter", sep="\n")
         choix = input("Que souhaitez vous faire ? ")
         match choix:
             case "d":
-                print("\n--------------------\nRecherche de livre\n--------------------")
-                livre = rechercher_livre()
-                connexion, curseur = ouvrir_connexion()
-                curseur.execute("SELECT date_ret FROM Emprunt WHERE isbn LIKE ?", (livre[3],))
-                emprunt = curseur.fetchone()
-                fermer_connexion(connexion)
-
-                print("\nLe livre intitulé '" + str(livre[0]) + "' de " + str(livre[1]) + ", qui a été publié en "
-                      + str(livre[2]) + " sous l'ISBN " + str(livre[3]) +
-                      " à été découvert dans le centre de documentation et d'information.")
-                if emprunt is None:
-                    print("Il n'est actuellement pas emprunté.")
-                else:
-                    print("Il est actuellement emprunté jusqu'au " + emprunt[0])
-                input("\nRetourner au menu (entrée) >")
+                rechercher()
             case "e":
-                print("\n-------\nEmprunt\n-------")
-                livre = rechercher_livre()
-                date = (datetime.date.today() + datetime.timedelta(weeks=3)).strftime('%Y-%m-%d')
-                connexion, curseur = ouvrir_connexion()
-                curseur.execute("SELECT date_ret FROM Emprunt WHERE isbn LIKE ?", (livre[3],))
-                emprunt = curseur.fetchone()
-                if emprunt is None:
-                    curseur.execute("INSERT INTO Emprunt VALUES (?, ?, ?)", (livre[3], num_etu, date))
-                else:
-                    print("Ce livre est déjà emprunté (jusqu'au " + emprunt[0] + ").")
-                fermer_connexion(connexion)
-                input("\nRetourner au menu (entrée) >")
+                emprunter(num_etu)
             case "r":
-                pass
+                rendre()
             case "a":
                 pass
             case "s":
@@ -70,13 +37,86 @@ def main():
             case "p":
                 pass
             case "q":
-                stop = True
+                break  # Arrêter la boucle While
             case _:
                 print("Je n'ai pas compris...")
     print("\nSortie du programme. Au revoir !")
 
 
-def rechercher_livre():
+def rechercher():
+    print("\n------------------\nRecherche de livre\n------------------")
+
+    # Procédure de recherche du livre dans la base de données
+    livre = afficher_resultats(recherche_livre(), ["#", "Titre", "Auteur.e", "Année", "ISBN"])
+
+    # Récupération de la date de retour de l'emprunt (s'il y en a un)
+    connexion, curseur = ouvrir_connexion()
+    curseur.execute("SELECT date_ret FROM Emprunt WHERE isbn LIKE ?", (livre[3],))
+    emprunt = curseur.fetchone()
+    connexion.close()
+
+    # Affichage des informations sur le livre et l'emprunt en cours
+    message = "\nLe livre intitulé '{0}' de {1}, qui a été publié en {2} sous l'ISBN {3} a été découvert dans le centre de documentation et d'information."
+    message += "\nIl n'est actuellement pas emprunté." if not emprunt else "\nIl est actuellement emprunté jusqu'au {4}."
+    print(message.format(livre[0], livre[1], livre[2], livre[3], emprunt[0] if emprunt else ""))
+
+    # Attente de l'utilisateur avant de retourner au menu
+    input("\nRetourner au menu (entrée) >")
+
+
+def emprunter(num_etu):
+    print("\n-------\nEmprunt\n-------")
+
+    # Procédure de recherche du livre dans la base de données
+    livre = afficher_resultats(recherche_livre(), ["#", "Titre", "Auteur.e", "Année", "ISBN"])
+
+    # Récupération de la date de retour de l'emprunt (s'il y en a un)
+    connexion, curseur = ouvrir_connexion()
+    curseur.execute("SELECT date_ret FROM Emprunt WHERE isbn LIKE ?", (livre[3],))
+    emprunt = curseur.fetchone()
+
+    if not emprunt:
+        date_retour = (datetime.date.today() + datetime.timedelta(weeks=3)).strftime('%Y-%m-%d')
+        curseur.execute("INSERT INTO Emprunt VALUES (?, ?, ?)", (livre[3], num_etu, date_retour))
+        connexion.commit()
+        print(f"\nVous avez correctement emprunté le livre intitulé '{livre[0]}' de {livre[1]} jusqu'au {date_retour}")
+    else:
+        print(f"\nCe livre est déjà emprunté (jusqu'au {emprunt[0]}).", end="")
+    connexion.close()
+
+    # Attente de l'utilisateur avant de retourner au menu
+    input("\nRetourner au menu (entrée) >")
+
+
+def rendre():
+    print("\n--------------\nRendre un livre\n--------------")
+
+    eleve = input("Saisissez votre numéro d'étudiant : ")
+
+    # Récupération des emprunts de l'élève (s'il y en a)
+    connexion, curseur = ouvrir_connexion()
+    curseur.execute("SELECT titre, nom, annee, isbn, date_ret FROM Emprunt "
+                    "JOIN Livre using (isbn)"
+                    "JOIN Ecrire using(isbn) "
+                    "JOIN Auteur using (a_id)"
+                    "WHERE num_etu LIKE ?", (eleve,))
+    emprunts = curseur.fetchall()
+    connexion.close()
+
+    if emprunts:
+        livre = afficher_resultats(emprunts, ["#", "Titre", "Auteur.e", "Année", "ISBN", "Date retour"])
+        connexion, curseur = ouvrir_connexion()
+        curseur.execute("DELETE FROM Emprunt WHERE isbn LIKE ?", (livre[3],))
+        connexion.commit()
+        connexion.close()
+    else:
+        print("\nCet.te élève.e n'a actuellement aucun emprunt de livre.")
+
+    # Attente de l'utilisateur avant de retourner au menu
+    input("\nRetourner au menu (entrée) >")
+
+
+def recherche_livre():
     titre = "%" + input("Titre : ") + "%"
     auteur = "%" + input("Auteur.e : ") + "%"
     annee = "%" + input("Année : ") + "%"
@@ -89,41 +129,35 @@ def rechercher_livre():
                     "WHERE titre LIKE ? AND nom LIKE ? AND annee LIKE ? AND isbn LIKE ?",
                     (titre, auteur, annee, isbn))
     resultats = curseur.fetchall()
-    fermer_connexion(connexion)
-
-    # S'il existe plusieurs livres avec les champs recherchés
-    nombre_resultats = len(resultats)
-    if nombre_resultats > 1:
-        table = PrettyTable()
-        table.field_names = ["#", "Titre", "Auteur.e", "Année", "ISBN"]
-        for i, rows in enumerate(resultats):
-            table.add_rows([[i + 1] + list(rows)])
-        print("\n", table, "\nPlusieurs livres ont étés trouvés avec vos paramètres de recherche.")
-        stop1 = False
-        while not stop1:
-            choix = input("Préciser le livre recherché avec son numéro : ")
-            try:
-                nombre = int(choix)
-                if 0 <= nombre <= nombre_resultats:
-                    stop1 = True
-                    return resultats[nombre - 1]
-                print("\nVeuillez saisir un numéro compris entre 1 et " + str(nombre_resultats))
-            except ValueError:
-                print("\nVeuillez saisir un numéro correct.")
-    elif nombre_resultats == 1:
-        return resultats[0]
-    else:
+    connexion.close()
+    if not resultats:
         print("\nAucun livre n'a été trouvé avec vos paramètres de recherche, veuillez réessayer.")
-        return rechercher_livre()
+        return recherche_livre()
+    return resultats
+
+
+def afficher_resultats(resultats, noms_champs):
+    nombre_resultats = len(resultats)
+    if nombre_resultats == 1:
+        return resultats[0]
+    table = PrettyTable()
+    table.field_names = noms_champs
+    table.add_rows([[i + 1, *rows] for i, rows in enumerate(resultats)])
+    print("\n", table, "\nPlusieurs livres ont étés trouvés avec vos paramètres de recherche.")
+    while True:
+        choix = input("Préciser le livre recherché avec son numéro indiqué dans la liste : ")
+        try:
+            nombre = int(choix)
+            if 0 <= nombre <= nombre_resultats:
+                return resultats[nombre - 1]
+            print("\nVeuillez saisir un numéro compris entre 1 et", str(nombre_resultats))
+        except ValueError:
+            print("\nVeuillez saisir un numéro correct.")
 
 
 def ouvrir_connexion():
     connexion = sqlite3.connect('cdi.db')
     return connexion, connexion.cursor()
-
-
-def fermer_connexion(connexion):
-    connexion.close()
 
 
 main()
